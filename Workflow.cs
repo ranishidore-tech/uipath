40a79a854e134053c3207fc3168905cb8070663f
Rani Shidore <ranishidore@gmail.com>
2025-11-19 20:10:15 +0530

    Git hub init

---
+++ Workflow.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using UiPath.Activities.System.Jobs.Coded;
+using UiPath.CodedWorkflows;
+using UiPath.Core;
+using UiPath.Core.Activities.Storage;
+using UiPath.Orchestrator.Client.Models;
+
+namespace RpaTraining
+{
+    public class Workflow : CodedWorkflow
+    {
+        [Workflow]
+        public void Execute()
+        {
+            // To start using services, use IntelliSense (CTRL + Space) to discover the available services:
+            // e.g. system.GetAsset(...)
+
+            // For accessing UI Elements from Object Repository, you can use the Descriptors class e.g:
+            // var screen = uiAutomation.Open(Descriptors.MyApp.FirstScreen);
+            // screen.Click(Descriptors.MyApp.FirstScreen.SettingsButton);
+        }
+    }
+}
